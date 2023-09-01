5a3bf4550178fa21805a846f70c2c2dbcf82fa29
Viktor Mehl <vikt2553@mail.hansenberg.dk>
2023-09-01 14:14:45 +0200

    Player movement + shooting

--- Assets/scripts/Projectile.cs
+++ Assets/scripts/Projectile.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+
+    public float damage = 5f;
+    public float range = 5f;
+
+
+
     void Update()
     {
-        
+
+
+        transform.Translate(Vector2.up * Time.deltaTime * 5f);
+
+
+
+        if (transform.position.y > range)
+        {
+            DestroyProjectile();
+        }
+
     }
+
+    private void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+
 }
