f43e354fbe8b66f5e47bf9102b3ab40ca0a0919e
Tobias Christiansen <tobi499a@mail.hansenberg.dk>
2023-09-04 10:05:01 +0200

    Lidt porjeksdftil

--- Assets/scripts/Projectile.cs
+++ Assets/scripts/Projectile.cs
@@ -8,13 +8,17 @@ public class Projectile : MonoBehaviour
 
     public float damage = 5f;
     public float Range = 1f;
+    public Enemy enemy;
 
+    void start()
+    {
+        Vector2 displacement = enemy.transform.position - transform.position;
+        displacement = displacement.normalized;
 
+    }
 
     void Update()
     {
-
-
         transform.Translate(Vector2.up * Time.deltaTime * 5f);
         Destroy(gameObject, Range);
 
