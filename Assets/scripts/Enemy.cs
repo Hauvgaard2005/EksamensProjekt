14b3fc8fc2a64f0b58c5985a2e4289f054212ea9
Hauvgaard <math214x@mail.hansenberg.dk>
2023-09-04 09:11:53 +0200

    Enemy og Minion update

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -7,12 +7,17 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        private float HP;
+        private float speed;
+        public float damage;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(HP <= 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
