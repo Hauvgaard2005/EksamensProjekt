fae3d38e9a1be95d8e8c71cd72d627c98077ef52
Tobias Christiansen <tobi499a@mail.hansenberg.dk>
2023-09-11 10:44:16 +0200

    Camera

--- Assets/scripts/Game.cs
+++ Assets/scripts/Game.cs
@@ -8,13 +8,13 @@ public class Game : MonoBehaviour
     public static Game Instance;
     [SerializeField] private GameObject enemyPrefab;
     [SerializeField] private int numberOfEnemies = 5;
-    private float spawnRadius = 10.0f;
+    private float spawnRadius = 100.0f;
     public Player SpawnedPlayer;
     public GameObject playerPrefab;
 
     public void Awake()
     {
-        if(Instance == null)
+        if (Instance == null)
         {
             Instance = this;
         }
@@ -30,6 +30,7 @@ public class Game : MonoBehaviour
         GameObject go = Instantiate(playerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
         SpawnedPlayer = go.GetComponent<Player>();
 
+        Camera.main.transform.SetParent(SpawnedPlayer.transform);
 
         spawnEnemies();
 
@@ -40,8 +41,14 @@ public class Game : MonoBehaviour
 
         for (int i = 0; i < numberOfEnemies; i++)
         {
-            Vector2 randomCircle = UnityEngine.Random.insideUnitCircle * spawnRadius;
-            Vector3 randomSpawnPoint = new Vector3(randomCircle.x, 0, randomCircle.y) + Game.Instance.SpawnedPlayer.transform.position;
+            Vector2 asd = UnityEngine.Random.insideUnitCircle;
+            Debug.Log(asd);
+
+            Vector2 randomCircle = asd * spawnRadius;
+            Debug.Log(randomCircle);
+
+            Vector3 randomSpawnPoint = new Vector3(randomCircle.x, randomCircle.y, 0) + Game.Instance.SpawnedPlayer.transform.position;
+            Debug.Log(randomSpawnPoint);
 
             Instantiate(enemyPrefab, randomSpawnPoint, Quaternion.identity);
         }
