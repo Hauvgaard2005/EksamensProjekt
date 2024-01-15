d7b6b1c30b3afb16f435e50f54852250a5947e45
Viktor Mehl <vikt2553@mail.hansenberg.dk>
2024-01-15 15:39:57 +0100

    Spec

--- Assets/scripts/Projectiles/SpecialAttack.cs
+++ Assets/scripts/Projectiles/SpecialAttack.cs
@@ -7,13 +7,17 @@ public class SpecialAttack : MonoBehaviour
     private Vector2 mousePos;
     private Rigidbody2D rb;
 
+    public float speed = 10f;
+    public float range = 5f;
+    private float timer = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Debug.Log(mousePos);
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = (mousePos - (Vector2)transform.position).normalized * 10f;
+        rb.velocity = (mousePos - (Vector2)transform.position).normalized * speed;
 
     }
 
@@ -21,13 +25,26 @@ public class SpecialAttack : MonoBehaviour
     void Update()
     {
 
+        timer += Time.deltaTime;
+        if (timer >= range)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision == Game.Instance.NearestEnemy.GetComponent<Collider2D>())
+        if (collision.gameObject.GetComponent<Enemy>() != null)
         {
             Destroy(gameObject);
         }
+
     }
 }
